5ca62c7318befff8e6b4c3e9a5bfa5b8a31fccd2
billchenchina <billchenchina2001@gmail.com>
2021-02-09 02:37:46 +0800

    Update dependencies
    Signed-off-by: billchenchina <billchenchina2001@gmail.com>

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,8 @@
-const { injectBabelPlugin } = require("react-app-rewired");
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-    ["import", { libraryName: "antd", libraryDirectory: "es", style: "css" }],
-    config
-  );
-  return config;
-};
+const { override, fixBabelImports } = require("customize-cra");
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  })
+);
