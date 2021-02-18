7502198e5beb08403530efb3c9f8b15a99f567fb
jubgjf <guanjiannan@outlook.com>
2021-02-18 19:53:14 +0800

    add doc navigation menu
    - parse json file as navigation menu items
    - add router link to menu
    items

--- src/components/doc/docPage.js
+++ src/components/doc/docPage.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import { Layout, Menu, Breadcrumb, Row, Col } from 'antd';
+import { Link } from "react-router-dom";
+import docMenu from './menu.json'
 import './docPage.css'
 
 const { SubMenu } = Menu;
 const { Content, Sider } = Layout;
 
 export default class DocPage extends Component {
+  /**
+   * 根据json文件递归生成文档导航菜单项目
+   */
+  generateMenuItems(source) {
+    return (
+      source.map((menu, index) => {
+        if (menu.children) {
+          return (
+            <SubMenu key={menu.key} title={menu.title}>
+              {this.generateMenuItems(menu.children)}
+            </SubMenu>
+          )
+        } else {
+          return (
+            <Menu.Item key={menu.key}>
+              <Link to={menu.path}>
+                {menu.title}
+              </Link>
+            </Menu.Item>
+          )
+        }
+      })
+    )
+  }
+
   render() {
     return (
       <Content style={{ padding: '0 50px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to={'/home'}>主页</Link>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <Link to={'/doc'}>帮助文档</Link>
+          </Breadcrumb.Item>
         </Breadcrumb>
         <Row>
           <Col md={4}>
@@ -28,34 +58,24 @@ export default class DocPage extends Component {
               >
                 <Menu
                   mode="inline"
-                  defaultSelectedKeys={['1']}
-                  defaultOpenKeys={['sub1']}
+                  defaultOpenKeys={['0']}
                   style={{ height: '100%' }}
                 >
-                  <SubMenu key="sub1" title="subnav 1">
-                    <Menu.Item key="1">option1</Menu.Item>
-                    <Menu.Item key="2">option2</Menu.Item>
-                    <Menu.Item key="3">option3</Menu.Item>
-                    <Menu.Item key="4">option4</Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="sub2" title="subnav 2">
-                    <Menu.Item key="5">option5</Menu.Item>
-                    <Menu.Item key="6">option6</Menu.Item>
-                    <Menu.Item key="7">option7</Menu.Item>
-                    <Menu.Item key="8">option8</Menu.Item>
-                  </SubMenu>
-                  <SubMenu key="sub3" title="subnav 3">
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                  </SubMenu>
+                  {this.generateMenuItems(docMenu)}
                 </Menu>
               </Sider>
             </Layout>
           </Col>
           <Col md={20}>
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+            <Content
+              style={{
+                padding: '0 24px',
+                minHeight: 280,
+                background: 'white'
+              }}
+            >
+              Content
+            </Content>
           </Col>
         </Row>
       </Content>
