d485926491793eea76771b793ab7192a8fcb7d19
jubgjf <guanjiannan@outlook.com>
2021-02-18 15:11:43 +0800

    add responsive navigation menu in doc page
    The width ratio of the navigation menu to the doc content is 4 to
    20.
    The navigation menu will automatically hide at 768px.

--- src/components/doc/docPage.js
+++ src/components/doc/docPage.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Row, Col } from 'antd';
+import './docPage.css'
+
+const { SubMenu } = Menu;
+const { Content, Sider } = Layout;
 
 export default class DocPage extends Component {
   render() {
     return (
-      <div>
-        <h1>This is doc page</h1>
-      </div>
-    );
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Row>
+          <Col md={4}>
+            <Layout
+              className="site-layout-background"
+              style={{ padding: '0' }}
+            >
+              <Sider
+                className="site-layout-background"
+                width={'100%'}
+                breakpoint={'md'}
+                collapsedWidth={0}
+              >
+                <Menu
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  defaultOpenKeys={['sub1']}
+                  style={{ height: '100%' }}
+                >
+                  <SubMenu key="sub1" title="subnav 1">
+                    <Menu.Item key="1">option1</Menu.Item>
+                    <Menu.Item key="2">option2</Menu.Item>
+                    <Menu.Item key="3">option3</Menu.Item>
+                    <Menu.Item key="4">option4</Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub2" title="subnav 2">
+                    <Menu.Item key="5">option5</Menu.Item>
+                    <Menu.Item key="6">option6</Menu.Item>
+                    <Menu.Item key="7">option7</Menu.Item>
+                    <Menu.Item key="8">option8</Menu.Item>
+                  </SubMenu>
+                  <SubMenu key="sub3" title="subnav 3">
+                    <Menu.Item key="9">option9</Menu.Item>
+                    <Menu.Item key="10">option10</Menu.Item>
+                    <Menu.Item key="11">option11</Menu.Item>
+                    <Menu.Item key="12">option12</Menu.Item>
+                  </SubMenu>
+                </Menu>
+              </Sider>
+            </Layout>
+          </Col>
+          <Col md={20}>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
+          </Col>
+        </Row>
+      </Content>
+    )
   }
 }
