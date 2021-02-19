10aed377ed2632ad8f55c949832820d24c0e6beb
billchenchina <billchenchina2001@gmail.com>
2021-02-19 20:41:35 +0800

    Switch to HashRouter
    Signed-off-by: billchenchina <billchenchina2001@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
