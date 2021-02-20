12238ee4195c6fe434e11470935c2e95d7333961
jubgjf <guanjiannan@outlook.com>
2021-02-20 18:28:05 +0800

    add icon to buttons

--- src/components/homePage/sideCards/docCard.js
+++ src/components/homePage/sideCards/docCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
+import { BulbOutlined } from "@ant-design/icons";
 
 export default class DocCard extends Component {
   render() {
@@ -8,9 +9,11 @@ export default class DocCard extends Component {
       <div>
         <h2>帮助文档</h2>
         <p>配置文件使用方法</p>
-        <Button type="primary">
-          <Link to={"/doc/docHome"}>帮助文档</Link>
-        </Button>
+        <Link to={"/doc/docHome"}>
+          <Button type="primary" icon={<BulbOutlined />}>
+            帮助文档
+          </Button>
+        </Link>
       </div>
     );
   }
